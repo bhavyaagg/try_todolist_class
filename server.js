b513b101d0cfde542e4cc9a181cc58bc6aaa8185
Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>
2017-12-31 01:12:05 +0530

    Statically hosting
    Signed-off-by: Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>

--- server.js
+++ server.js
@@ -3,18 +3,23 @@
  */
 
 const express = require('express');
+const path = require('path');
+
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded());
 
-app.get('/', (req, res, next) => {
+app.get('/get', (req, res, next) => {
   res.send("Get Working");
 });
 
 app.post('/post', (req, res, next) => {
   res.send("Post Working");
 });
+
+
+app.use(express.static(path.join(__dirname, 'public_static')));
 
 app.listen(4000, function () {
   console.log("Server listening on port 4000");
